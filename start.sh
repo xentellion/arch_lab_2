c84549a374e549905318dabd2268bc8f99acc9b2
Xent <xentellion@xentellion-bots.ru>
2022-04-19 20:05:25 +0300

    typo

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo ./build/hl_mai_lab_02 --host=localhost --port=3306 --login=stud --password=stud --database=stud
+sudo ./build/hl_mai_lab_2 --host=localhost --port=3306 --login=stud --password=stud --database=stud
